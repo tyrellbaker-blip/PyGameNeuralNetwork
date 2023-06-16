ad90b4730cf55cd113e01e3d44339f70a6167563
prameet <prameet.guha@gmail.com>
2023-06-16 19:08:39 -0400

    I have some questions written in the TODO at the bottom. Can you please help me?

--- snake.py
+++ snake.py
@@ -93,12 +93,11 @@ inclusive), multiplied by 10.
 The second element should be a random integer between 1 and window_y//10 (
 inclusive), multiplied by 10.
 Set fruit_spawn to True.
-
-
 """
 
 # create a function to spawn the fruit
 """
+DONE
 spawn_fruit()
 Define the function with no inputs or outputs.
 Access the global variables fruit_position and fruit_spawn.
@@ -108,13 +107,17 @@ inclusive), multiplied by 10.
 The second element should be a random integer between 1 and window_y//10 (
 inclusive), multiplied by 10.
 Set fruit_spawn to True.
-
-
-
 """
+def spawn_fruit():
+    global fruit_spawn
+    global fruit_position
+    fruit_position = [random.randrange(1, (window_x // 10)) * 10,
+                  random.randrange(1, (window_y // 10)) * 10]
+    fruit_spawn = True
 
 # create a function to draw the snake
 """
+HELP
 draw_snake()
 Define the function with no inputs or outputs.
 Draw a black rectangle on the game_window using game_window.fill() to clear 
@@ -125,14 +128,18 @@ Alternatively, you may want to draw an image of the snake instead of
 rectangles.
 Draw a white rectangle on game_window using pygame.draw.rect() to represent 
 the fruit.
-
-
-
 """
+def draw_snake():
+    game_window.fill(black)
+    for block in starting_body:
+        pygame.draw.rect(green)
+    for fruit in fruit_position:
+        pygame.draw.rect(white)
 
 # create a function to check if the game is over
 
 """
+HELP
 check_game_over()
 Define the function with no inputs or outputs.
 Access the global variables snake_position, window_x, and window_y.
@@ -140,11 +147,18 @@ If the x-coordinate of snake_position is less than 0, or greater than
 window_x minus 10, call the game_over() function.
 If the y-coordinate of snake_position is less than 0, or greater than 
 window_y minus 10, call the game_over() function.
-
 """
-
+def check_game_over():
+    global snake_position
+    global window_x
+    global window_y
+    if snake_position[1] < 0 | snake_position[1] > (window_y - 10):
+        #game_over()
+    if snake_position[1] < 0 | snake_position[1] > (window_x - 10):
+        # game_over()
 # create a function to check if there is a collision
 """
+DONE
 check_collision()
 Define the function with no inputs or outputs.
 Access the global variables snake_body and snake_position.
@@ -152,13 +166,17 @@ Use a for loop to iterate over each block in snake_body except the first one
 (since the head can't collide with itself).
 If the position of the current block matches the position of snake_position, 
 call the game_over() function.
-
-
-
 """
+def check_collision():
+    global snake_body
+    global snake_position
+    for block in snake_body[1:]:
+        if snake_position == snake_position[0]:
+            #game_over()
 
 # create a function to show the score
 """
+DONE
 show_score()
 Define the function with no inputs or outputs.
 Access the global variables score, game_window, and white.
@@ -167,6 +185,29 @@ Use the render() method on the font object to create a text surface object.
 Use the get_rect() method on the text surface object to get a rectangle that 
 encloses the text.
 Use the blit() method on the game window to display the text surface.
-
-
 """
+def show_score():
+    global score
+    global game_window
+    global white
+    font = pygame.font.SysFont()
+    text = font.render()
+    text.get_rect()
+    game_window.blit()
+
+# TODO:
+#   Thank you so much for these instructions! I just had a few questions on the code
+#   since I couldn't figure some stuff out:
+#   -----
+#   On line 84, the spawn_fruit function is described twice and the function
+#   the snake body growing mechanism isn't. I'm not really sure how to write the body
+#   growing function.
+#   -----
+#   Also, should there be a separate game_over() function? Or is there already
+#   one built in? I don't see the instructions to make it, so I was wondering.
+#   -----
+#   Lastly, when is says snake_position in line 146, was that supposed to be
+#   declared in the beginning? I checked and saw I never had a variable called that.
+#   -----
+#   Can you please check the other function in my code to make sure they are correct?
+#   Thanks!
